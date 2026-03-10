97d584837e47feef25430b0fe7cd393e34b88314
mia <ledikoska12@gmail.com>
2026-03-10 11:56:42 +0100

    tp4 PanierService.php

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\PanierService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -23,8 +24,13 @@ final class PanierController extends AbstractController
 
     #[Route('ajouter/{idProduit}/{quantite}', name: 'app_panier_ajouter')]
 
-    public function ajouter(int $idProduit, int $quantite): Response
+    public function ajouter(int $idProduit, int $quantite, PanierService $panierService): Response
     {
+
+        //
+        $panierService->ajouterProduit($idProduit, $quantite);
+
+        //
         return $this->redirectToRoute('app_panier_index');
 
             }
